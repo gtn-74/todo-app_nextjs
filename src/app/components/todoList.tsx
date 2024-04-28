221feedf3b80e95381db6098c8e23ccecb86497a
Ryosuke Ogitnai <gtn74@gitani.local>
2024-04-28 16:51:42 +0900

    🎨 コンポーネントレイアウト作成
    addTask todoListコンポーネントレイアウト作成

--- src/app/components/todoList.tsx
+++ src/app/components/todoList.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const TodoList = () => {
   return (
-    <div>todoList</div>
-  )
-}
+    <>
+      {/* listが増えていく */}
+      <ul className="space-y-3">
+        <li className="mb-4 flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow">
+          <span className="text-gray-700">Task 1</span>
+          <div>
+            <button className="text-green-500 mr-3">Edit</button>
+            <button className="text-red-500 ">Delete</button>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+};
 
-export default TodoList
+export default TodoList;
