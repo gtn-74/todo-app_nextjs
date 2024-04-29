e6bb39eae2ce3bd9e1541a6563373127d4620638
Ryosuke Ogitnai <gtn74@gitani.local>
2024-04-29 13:09:46 +0900

    ⚡️ Add api実装
    Create addApi

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,24 @@
- export type Task = {
+export type Task = {
   id: string;
   text: string;
 };
 
 export const getAllTodos = async (): Promise<Task[]> => {
-    // SSR:{ cache: "no-store" }
-  const res = await fetch("http://localhost:3001/task", { cache: "no-store" });
+  // SSR:{ cache: "no-store" }
+  const res = await fetch("http://localhost:3001/task", { cache: "no-store" }); //キャッシュへのdata保存をしない
   const todos = res.json();
 
   return todos;
+};
+
+export const addTodos = async (todo:Task): Promise<Task> => {
+  const res = await fetch("http://localhost:3001/task", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },body:JSON.stringify(todo)
+  });
+  const newTodo = res.json();
+
+  return newTodo;
 };
 
 /*
--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
+// hooksを使う場合、use clientの記述が必要
+"use client";
+import { addTodos } from "@/api";
+import React, { ChangeEvent, FormEvent } from "react";
 
 const AddTask = () => {
-  return (
-    <form className='mb-4 space-y-3'>
-      <input type="text" className='w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400'/>
-      <button className='w-full px-4 py-2 text-white bg-blue-500 rounded transform hover: bg-blue-400 hover:scale-95 duration-200'>Add task</button>
-    </form>
-  )
-}
+  const [taskTitle, setTaskTitle] = React.useState("");
+  
 
-export default AddTask
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    await addTodos({ id: "4", text: taskTitle });
+
+    setTaskTitle("");
+  };
+
+  return (
+    <form className="mb-4 space-y-3" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="w-full border px-4 py-2 rounded-lg focus:outline-none focus:border-blue-400"
+        // inputに入力した値がチェンジする度、更新関数に保存
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setTaskTitle(e.target.value)
+        }
+        value={taskTitle}
+      />
+      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover: bg-blue-400 hover:scale-95 duration-200">
+        Add task
+      </button>
+    </form>
+  );
+};
+
+export default AddTask;
