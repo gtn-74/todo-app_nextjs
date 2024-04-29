1b3dcd167396271b1784eaa3f3219416d54fe48b
Ryosuke Ogitnai <gtn74@gitani.local>
2024-04-29 16:03:50 +0900

    ⚡️ CreateApi put&delete
    put,deleteAPIとclean code

--- src/api.ts
+++ src/api.ts
@@ -2,23 +2,47 @@ export type Task = {
   id: string;
   text: string;
 };
-
+// GETAPI
 export const getAllTodos = async (): Promise<Task[]> => {
-  // SSR:{ cache: "no-store" }
   const res = await fetch("http://localhost:3001/task", { cache: "no-store" }); //キャッシュへのdata保存をしない
   const todos = res.json();
 
   return todos;
 };
 
-export const addTodos = async (todo:Task): Promise<Task> => {
+// POSTAPI
+export const addTodos = async (todo: Task): Promise<Task> => {
   const res = await fetch("http://localhost:3001/task", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },body:JSON.stringify(todo)
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(todo),
   });
   const newTodo = res.json();
 
   return newTodo;
+};
+
+// PUTAPI
+export const editTodos = async (id: string, newText: string): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/task/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: newText }),
+  });
+  const updatedTodo = res.json();
+
+  return updatedTodo;
+};
+
+// deleteAPI
+export const deleteTodos = async (id: string): Promise<Task> => {
+  const res = await fetch(`http://localhost:3001/task/${id}`, {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+  });
+  const deleteTodo = res.json();
+
+  return deleteTodo;
 };
 
 /*
--- src/app/components/todo.tsx
+++ src/app/components/todo.tsx
@@ -1,4 +1,5 @@
-import { Task } from "@/api";
+"use client";
+import { Task, deleteTodos, editTodos } from "@/api";
 import React from "react";
 
 type TodoProps = {
@@ -6,15 +7,69 @@ type TodoProps = {
 };
 
 const Todo = ({ todo }: TodoProps) => {
+  // 入力値を更新する
+  const [idSaving, setIdSaving] = React.useState(todo.text);
+
+  // editボタン
+  const [idEditing, setIdEditing] = React.useState(false);
+  const handleEdit = async () => {
+    setIdEditing(true);
+  };
+
+  // saveボタン
+  const handleSave = async () => {
+    await editTodos(todo.id, idSaving);
+    setIdEditing(false);
+  };
+
+  // html属性にアクセスできるhooks
+  const ref = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    if (idEditing) {
+      // null許容のため、オプショナルチェーンで回避
+      ref.current?.focus();
+    }
+  }, [idEditing]);
+
+  // deleteボタン
+  const handleDelete = async () => {
+    await deleteTodos(todo.id);
+  };
   return (
     <li
       key={todo.id}
       className="mb-4 flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
     >
-      <span className="text-gray-700">{todo.text}</span>
+      {/* inputフォームとテキストが、editのbooleanでスイッチする */}
+      {idEditing ? (
+        <input
+          ref={ref}
+          type="text"
+          className="mr-2 py-1 px-2 rounded border-gray-400 border"
+          value={idSaving}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setIdSaving(e.target.value);
+          }}
+        />
+      ) : (
+        <span className="text-gray-700">{todo.text}</span>
+      )}
+
       <div>
-        <button className="text-green-500 mr-3">Edit</button>
-        <button className="text-red-500 ">Delete</button>
+        {/* inputフォームとテキストが、editのbooleanでスイッチする */}
+        {idEditing ? (
+          <button className="text-green-500 mr-3" onClick={handleSave}>
+            Save
+          </button>
+        ) : (
+          <button className="text-green-500 mr-3" onClick={handleEdit}>
+            Edit
+          </button>
+        )}
+        <button className="text-red-500" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </li>
   );
