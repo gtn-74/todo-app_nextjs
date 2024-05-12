8187497cb7e1775f70957c817a69977c615b7c05
Ryosuke Ogitani <gtn.r74@gmail.com>
2024-05-12 19:53:53 +0900

    memo追加

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -23,3 +23,19 @@ export const handlers = [
     );
   }),
 ];
+// msw2.0
+// https://zenn.dev/keitakn/scraps/2ca70305a71847
+// https://qiita.com/eito_katagiri-litalico/items/9704229de4b1eee86715
+// https://zenn.dev/keitakn/scraps/2ca70305a71847
+// https://zenn.dev/ryo_kawamata/articles/mock-api-server-with-msw
+// https://qiita.com/KokiSakano/items/62180f334a6137d429b5#deplay
+// https://mswjs.io/blog/introducing-msw-2.0/
+// https://v1.mswjs.io/docs/getting-started/mocks/rest-api
+
+
+// jest mockを使わずに非同期リクエストてストを書く
+// https://zenn.dev/ryo_kawamata/articles/introduce-mock-service-worker
+// https://mswjs.io/docs/migrations/1.x-to-2.x/
+
+// msw1.3.2
+// https://zenn.dev/collabostyle/articles/136b117d2893c0
